a9eaaa6a1d89d5ffbcdbef225b7d75ba9e56f2e7
Sabogal22 <nsabogalgaitan@gmail.com>
2026-02-20 18:59:07 -0500

    Actualiza el ultimo inicio de sesion cada vez que inicia sesion

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -2,16 +2,39 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import update_last_login
 from .serializers import UserSerializer, EmployeeSerializer
+from typing import cast
+from django.contrib.auth.base_user import AbstractBaseUser
 from .models import User
 
-class CurrentUserView(APIView):
-  permission_classes = [IsAuthenticated]
 
-  def get(self, request):
-    serializer = UserSerializer(request.user)
-    return Response(serializer.data)
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        user = cast(AbstractBaseUser, self.user)
+
+        update_last_login(type(user), user)
+
+        return data
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
 
 class EmployeeListView(ListAPIView):
-  serializer_class = EmployeeSerializer
-  queryset = User.objects.exclude(role="admin")
+    serializer_class = EmployeeSerializer
+    queryset = User.objects.all()
